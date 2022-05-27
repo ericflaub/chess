76c0e88e12b9dd3407a8d0197047ab5f48edfdb1
ericflaub <91358141+ericflaub@users.noreply.github.com>
2022-05-27 15:31:47 -0400

    Initial commit

---
+++ data_pull.py
@@ -0,0 +1,36 @@
+import berserk
+import datetime
+from datetime import date
+session = berserk.TokenSession('lip_5wiPU7gzAZ9rdfDXuGIb')
+client = berserk.Client(session=session)
+
+eric = client.account.get()
+start_date = eric['createdAt']
+end_date = date.today()
+
+client.games.export_by_player('icererci', since=start_date, until=end_date)
+games = list(_)
+error_count = []
+outcome_series = []
+for i in games:
+    a = i
+    if a['rated'] == True:
+        try:
+            w_player = a['players']['white']['user']['name']
+            b_player = a['players']['black']['user']['name']
+            w_change = a['players']['white']['ratingDiff']
+            b_change = a['players']['black']['ratingDiff']
+
+
+            if (w_player == 'icererci' and w_change > 0) or (b_player == 'icererci' and b_change > 0):
+                outcome = 1
+            elif (w_player == 'icererci' and w_change == 0) or (b_player == 'icererci' and b_change == 0):
+                outcome = 3
+            else:
+                outcome = 0
+            outcome_series.append(outcome)
+        except:
+            error_count.append('oh no')
+            pass
+len(outcome_series)
+error_count
